36525cf7c29699f5dcf879199ed2bed5579ece6a
anandkumarnishant65 <anandkumarnishant65@gmail.com>
2023-08-19 19:07:03 +0530

    Delete Data LocalStorage

--- j.js
+++ j.js
@@ -1,19 +1,42 @@
 function store(){ //stores items in the localStorage
   var name = document.getElementById('name').value;
-  var phone = document.getElementById('phn').value;
+  var phn = document.getElementById('phn').value;
   var key = document.getElementById('key').value;
 
-  const deatails = {
+  const details = {
       name: name,
-      phone: phone,
-      key: key
+      phn: phn,
   }
 
-  window.localStorage.setItem(key,JSON.stringify(deatails));  
+  window.localStorage.setItem(key,JSON.stringify(details));  
   //converting object to string
 }
 
+function retrieveRecords(){ //retrieves items in the localStorage
+  var key = document.getElementById('retrieveKey').value; //gets key from user
+  console.log("retrive records");
+  var records = window.localStorage.getItem(key); //searches for the key in localStorage
+  var paragraph = document.createElement("p");
+  var infor = document.createTextNode(records);
+  paragraph.appendChild(infor);
+  var element = document.getElementById("retrieve");
+  element.appendChild(paragraph);
+}
+
+function removeItem(){ //deletes item from localStorage
+  var key = document.getElementById('removeKey').value; //gets key from user
+  localStorage.removeItem(key) //passes key to the removeItem method
+  console.log("remove items");
+}
+
+function clearStorage(){ //clears the entire localStorage
+  localStorage.clear()
+  console.log("clear records");
+}
 
 window.onload =function(){ //ensures the page is loaded before functions are executed.
   document.getElementById("detailsForm").onsubmit = store
+  document.getElementById("clearButton").onclick = clearStorage
+  document.getElementById("removeButton").onclick = removeItem
+  document.getElementById("retrieveButton").onclick = retrieveRecords
 }
