3e420760f632dfe23af7203b99ae098c1d9ac9d1
anandkumarnishant65 <anandkumarnishant65@gmail.com>
2023-08-19 19:23:54 +0530

    edit data in localStorage

--- j.js
+++ j.js
@@ -1,22 +1,22 @@
 function store(){ //stores items in the localStorage
   var name = document.getElementById('name').value;
-  var phn = document.getElementById('phn').value;
+  var phoneNumber = document.getElementById('phn').value;
   var key = document.getElementById('key').value;
 
-  const details = {
+  const deatails = {
       name: name,
-      phn: phn,
+      phoneNumber: phoneNumber,
   }
 
-  window.localStorage.setItem(key,JSON.stringify(details));  
+  window.localStorage.setItem(key,JSON.stringify(deatails));  
   //converting object to string
 }
 
 function retrieveRecords(){ //retrieves items in the localStorage
-  var key = document.getElementById('retrieveKey').value; //gets key from user
+  var key = document.getElementById('name').value; //gets key from user
   console.log("retrive records");
-  var records = window.localStorage.getItem(key); //searches for the key in localStorage
-  var paragraph = document.createElement("p");
+  var records = windows.localStorage.getItem(key); //searches for the key in localStorage
+  var paragraph = document.createElement("");
   var infor = document.createTextNode(records);
   paragraph.appendChild(infor);
   var element = document.getElementById("retrieve");
@@ -24,19 +24,18 @@ function retrieveRecords(){ //retrieves items in the localStorage
 }
 
 function removeItem(){ //deletes item from localStorage
-  var key = document.getElementById('removeKey').value; //gets key from user
-  localStorage.removeItem(key) //passes key to the removeItem method
-  console.log("remove items");
+    var key = document.getElementById('removeKey').value; //gets key from user
+    localStorage.removeItem(key) //passes key to the removeItem method
+    console.log("remove items");
 }
 
 function clearStorage(){ //clears the entire localStorage
-  localStorage.clear()
-  console.log("clear records");
+    localStorage.clear()
+    console.log("clear records");
 }
 
 window.onload =function(){ //ensures the page is loaded before functions are executed.
   document.getElementById("detailsForm").onsubmit = store
   document.getElementById("clearButton").onclick = clearStorage
   document.getElementById("removeButton").onclick = removeItem
-  document.getElementById("retrieveButton").onclick = retrieveRecords
 }
